69055cc755027303c594d993043d892690ab1bf8
francisco alejandro myers muñoz <franciscomyers@gmail.com>
2023-08-28 18:44:49 -0400

    Feature: Create app.js

---
+++ backend/app.js
@@ -0,0 +1,14 @@
+import express from "express";
+import bodyParser from "body-parser";
+import morgan from "morgan";
+import cors from "cors";
+// import routes from "./routes";
+
+export const server = express();
+
+server.use(cors());
+server.use(bodyParser.json());
+server.use(morgan("dev"));
+
+// routes
+// server.use("/", routes);
