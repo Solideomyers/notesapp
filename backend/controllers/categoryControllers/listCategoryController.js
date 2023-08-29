75274d67512187a31acef99383020906ae0c8223
francisco alejandro myers muñoz <franciscomyers@gmail.com>
2023-08-29 09:39:37 -0400

    Feature: Create controller by list category

---
+++ backend/controllers/categoryControllers/listCategoryController.js
@@ -0,0 +1,21 @@
+import { listCategoryServices } from "../../services/categoryServices";
+
+export const listCategoryController = async (req, res) => {
+  // console.log(title, content)
+  try {
+    const newCategory = await listCategoryServices();
+    if (!newCategory) {
+      return res.status(500).json({ success: false, message: "bad request" });
+    }
+    const response = {
+      success: true,
+      message: "List successfully",
+      data: newCategory,
+    };
+
+    return res.status(201).json(response);
+  } catch (error) {
+    // console.error('CategoryController', error);
+    res.status(500).json({ message: "failed to list Category" });
+  }
+};
