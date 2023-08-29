d08486624224115c20ab7fc331e11f29f911b745
francisco alejandro myers muñoz <franciscomyers@gmail.com>
2023-08-29 09:43:37 -0400

    Feature: Create controller by create note

---
+++ backend/controllers/noteControllers/createNoteController.js
@@ -0,0 +1,37 @@
+import { createNotesServices } from "../../services/noteServices/index.js";
+
+export const createNoteController = async (req, res) => {
+  // console.log(Note)
+  const { title, content, archived, actived, category } = req.body;
+  // console.log(title, content)
+  try {
+    const data = {
+      title,
+      content,
+      archived,
+      actived,
+      category,
+    };
+    if (!title || !content) {
+      return res.status(500).json({ success: false, message: "bad request" });
+    }
+    const newNote = await createNotesServices(
+      title,
+      content,
+      archived,
+      actived,
+      category
+    );
+
+    const response = {
+      success: true,
+      message: "Create successfully",
+      data: newNote,
+    };
+
+    return res.status(201).json(response);
+  } catch (error) {
+    // console.error('noteController', error);
+    res.status(500).json({ message: "failed to create note" });
+  }
+};
